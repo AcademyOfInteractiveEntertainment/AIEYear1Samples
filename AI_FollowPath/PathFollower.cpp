3fbacfd1c130ccd843c6a9ee27c1d597f769b95a
Mike Cooper <drmike@fundamentzero.com>
2022-01-03 16:44:14 +1030

    FollowPath example updated to use glm::vec2 instead of Raylib Vector2

--- AI_FollowPath/PathFollower.cpp
+++ AI_FollowPath/PathFollower.cpp
@@ -1,4 +1,5 @@
 #include "PathFollower.h"
+#include "raylib.h"
 
 PathFollower::PathFollower()
 {
@@ -15,13 +16,13 @@ PathFollower::PathFollower()
 void PathFollower::Update(float deltaTime)
 {
     int segment;
-    Vector2 closestPoint = GetClosestPointOnPath(segment);
-    float distAhead = Vector2Length(m_velocity);
-    Vector2 target = GetPointAlongPath(closestPoint, segment, distAhead);
+    glm::vec2 closestPoint = GetClosestPointOnPath(segment);
+    float distAhead = glm::length(m_velocity);
+    glm::vec2 target = GetPointAlongPath(closestPoint, segment, distAhead);
 
     // find the unit vector direction to the target
-    Vector2 toTarget = Vector2Subtract(target, m_position);
-    toTarget = Vector2Normalize(toTarget);
+    glm::vec2 toTarget = target - m_position;
+    toTarget = glm::normalize(toTarget);
 
     m_debugClosest = closestPoint;
     m_debugTarget = target;
@@ -31,9 +32,9 @@ void PathFollower::Update(float deltaTime)
 
     /*if (segment >= 0)
     {
-        Vector2 localForwards = Vector2Normalize(Vector2Subtract(m_path[segment == (int)m_path.size()-1 ? 0 : segment + 1], m_path[segment]));
-        Vector2 toTarget = Vector2Normalize(Vector2Subtract(target, m_position));
-        float braking = Vector2DotProduct(localForwards, toTarget);
+        glm::vec2 localForwards = glm::vec2Normalize(glm::vec2Subtract(m_path[segment == (int)m_path.size()-1 ? 0 : segment + 1], m_path[segment]));
+        glm::vec2 toTarget = glm::vec2Normalize(glm::vec2Subtract(target, m_position));
+        float braking = glm::vec2DotProduct(localForwards, toTarget);
         if (braking < 0.1f)
             braking = 0.1f;
         topSpeed *= braking;
@@ -41,13 +42,13 @@ void PathFollower::Update(float deltaTime)
 
     // friction - we want this to balance out acceleration when we're moving at speed.
     // so this should provide a force equal to -acceleration when moving at top speed.
-    m_velocity = Vector2Add(m_velocity, Vector2Scale(m_velocity, -m_acceleration / m_speed * deltaTime));
+    m_velocity *= (1 - m_acceleration / m_speed * deltaTime);
 
     // accelerate towards target point
-    m_velocity = Vector2Add(m_velocity, Vector2Scale(toTarget, m_acceleration * deltaTime));
+    m_velocity += toTarget * m_acceleration * deltaTime;
 
     // apply velocity
-    m_position = Vector2Add(m_position, Vector2Scale(m_velocity, deltaTime));
+    m_position += m_velocity * deltaTime;
 }
 
 void PathFollower::Draw()
@@ -55,8 +56,8 @@ void PathFollower::Draw()
     // draw the path in black
     for (int i = 0; i < (int)m_path.size(); i++)
     {
-        Vector2 start = m_path[i == 0 ? (int)m_path.size() - 1 : i - 1];
-        Vector2 end = m_path[i];
+        glm::vec2 start = m_path[i == 0 ? (int)m_path.size() - 1 : i - 1];
+        glm::vec2 end = m_path[i];
         DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, { 0,0,0,255 });
         DrawCircle((int)start.x, (int)start.y, 4, { 0,0,0,255 });
     }
@@ -73,31 +74,31 @@ void PathFollower::Draw()
 
 // for each segment on the path, find the closest point to the player
 // returns the closest point, and the segment index such that point lies between points [segment] and [segment + 1]
-Vector2 PathFollower::GetClosestPointOnPath(int& segment)
+glm::vec2 PathFollower::GetClosestPointOnPath(int& segment)
 {
     float closestDistance = FLT_MAX;
-    Vector2 closestPoint = m_position;
+    glm::vec2 closestPoint = m_position;
     segment = -1;
 
     // loop over each segment
     for (int i = 0; i < (int)m_path.size(); i++)
     {
-        Vector2 segmentStart = m_path[i];
-        Vector2 segmentEnd = m_path[i == (int)m_path.size() - 1 ? 0 : i + 1];
+        glm::vec2 segmentStart = m_path[i];
+        glm::vec2 segmentEnd = m_path[i == (int)m_path.size() - 1 ? 0 : i + 1];
 
-        Vector2 toPosition = Vector2Subtract(m_position, segmentStart);
-        Vector2 toEnd = Vector2Subtract(segmentEnd, segmentStart);
+        glm::vec2 toPosition = m_position - segmentStart;
+        glm::vec2 toEnd = segmentEnd - segmentStart;
 
         // project along the segment to get a value where 0 = level with start and 1 = level with end.
-        float alpha = Vector2DotProduct(toPosition, toEnd) / Vector2DotProduct(toEnd, toEnd);
+        float alpha = glm::dot(toPosition, toEnd) / glm::dot(toEnd, toEnd);
 
         // clamp that value
         if (alpha < 0) alpha = 0;
         if (alpha > 1) alpha = 1;
 
         // closest point on the line segment comes from lerping from start to end with our clamped alpha value
-        Vector2 point = Vector2Lerp(segmentStart, segmentEnd, alpha);
-        float distance = Vector2Distance(point, m_position);
+        glm::vec2 point = segmentStart * (1-alpha) + segmentEnd * alpha;
+        float distance = glm::distance(point, m_position);
 
         if (closestDistance > distance)
         {
@@ -112,17 +113,18 @@ Vector2 PathFollower::GetClosestPointOnPath(int& segment)
 
 // pos is a position on the path, preferably calculated using GetClosestPoint()
 // we step along the path segment by segment until we've reached the end or travelled (distanceAhead) metres
-Vector2 PathFollower::GetPointAlongPath(Vector2 pos, int segment, float distanceAhead)
+glm::vec2 PathFollower::GetPointAlongPath(glm::vec2 pos, int segment, float distanceAhead)
 {
-    Vector2 start = pos;
-    Vector2 end = m_path[segment == m_path.size() -1 ? 0 : segment + 1];
+    glm::vec2 start = pos;
+    glm::vec2 end = m_path[segment == m_path.size() -1 ? 0 : segment + 1];
     while (distanceAhead > 0)
     {
-        float remainingDistance = Vector2Distance(start, end);
+        float remainingDistance = glm::distance(start, end);
         if (distanceAhead <= remainingDistance)
         {
             // we won't reach the end node of this segment with our remaining distance, so lerp towards the end
-            return Vector2Lerp(start, end, distanceAhead / remainingDistance);
+            float alpha = distanceAhead / remainingDistance;
+            return start * (1 - alpha) + end *alpha;
         }
         else
         {
--- AI_FollowPath/PathFollower.h
+++ AI_FollowPath/PathFollower.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include "raymath.h"
+#include <glm/glm.hpp>
 
 class PathFollower
 {
@@ -9,21 +9,21 @@ public:
     void Update(float deltaTime);
     void Draw();
 
-    void AddPoint(Vector2 point) { m_path.push_back(point); }
+    void AddPoint(glm::vec2 point) { m_path.push_back(point); }
     void SetPosition(float x, float y) { m_position.x = x; m_position.y = y; }
 
 private:
     // array of points describing a closed path
-    std::vector<Vector2> m_path;
-    Vector2 m_position;
-    Vector2 m_velocity;
+    std::vector<glm::vec2> m_path;
+    glm::vec2 m_position;
+    glm::vec2 m_velocity;
     float m_speed;
     float m_acceleration;
 
-    Vector2 m_debugClosest;
-    Vector2 m_debugTarget;
+    glm::vec2 m_debugClosest;
+    glm::vec2 m_debugTarget;
 
-    Vector2 GetClosestPointOnPath(int& segment);
-    Vector2 GetPointAlongPath(Vector2 pos, int segment, float distanceAhead);
+    glm::vec2 GetClosestPointOnPath(int& segment);
+    glm::vec2 GetPointAlongPath(glm::vec2 pos, int segment, float distanceAhead);
 };
 
